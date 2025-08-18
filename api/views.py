f639bd2708eb418014ce2e4fb000b34e21f3fb41
kritikadhakal07 <kritikadhakal211@gmail.com>
2025-08-18 17:26:50 +0545

    serializer to featch and store data

---
+++ api/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+#Here we were manually serailizing the query set by converting them in list and
+#returning it as json response
+
+# def studentsView(request):
+#    students=Student.objects.all()
+#    student_list = list(students.values())
+#    return JsonResponse(student_list,safe=False)
+
+
+@api_view(['GET','POST'])
+def studentsView(request):
+   #  print(request)
+    """
+    This view function handles the HTTP GET request to get all the data
+    from the student table. It returns a JSON response containing the serialized
+    data.
+
+    Parameters:
+        request (HttpRequest): The HTTP request object.
+
+    Returns:
+        Response: The HTTP response object containing the serialized data.
+    """
+
+    if request.method == 'GET':
+        #Get all the data from the student table
+        students=Student.objects.all()
+        serializer = StudentSerializer(students,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        #Create a new student(Post)
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
